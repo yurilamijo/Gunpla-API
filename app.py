ae7927a6874a1bac59c5e121b91677a85f7fa061
yurilamijo <yurilamijo@hotmail.com>
2020-05-20 12:56:11 +0200

    JWT config cleanup

--- app.py
+++ app.py
@@ -3,11 +3,10 @@ import os
 from db import db
 from flask import Flask, jsonify
 from flask_restful import Api
-from flask_jwt_extended import JWTManager
 
+from jwt_config import jwt_init
 from resources.gunpla import Gunpla, GunplaList
 from resources.user import UserRegister, User, UserLogin, UserLogout, TokenRefresh
-from blacklist import BLACKLIST
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
@@ -22,52 +21,7 @@ api = Api(app)
 def create_tables():
     db.create_all()
 
-jwt = JWTManager(app)
-
-@jwt.token_in_blacklist_loader
-def check_token_blacklist(decrypted_token):
-    return decrypted_token['jti'] in BLACKLIST
-
-@jwt.user_claims_loader
-def add_claims_to_jwt(identity):
-    if identity == 1:
-        return {'is_admin': True}
-    return {'is_admin': False}
-
-@jwt.expired_token_loader
-def expired_token_callback():
-    return jsonify({
-        'message': 'The token has expired',
-        'error': 'token_expired'
-    }), 401
-
-@jwt.invalid_token_loader
-def invalid_token_callback(error):
-    return jsonify({
-        'description': 'Oi YAMERO',
-        'error': 'invalid_token'
-    }), 401
-
-@jwt.unauthorized_loader
-def unauthorized_token_callback():
-    return jsonify({
-        'desscription': 'Request does not contain an access token',
-        'error': 'authorization_required'
-    }), 401
-
-@jwt.needs_fresh_token_loader
-def token_not_fresh_callback():
-    return jsonify({
-        'desscription': 'Current token is not fresh',
-        'error': 'fresh_token_required'
-    }), 401
-
-@jwt.revoked_token_loader
-def revoked_token_callback():
-    return jsonify({
-        'desscription': 'The current token is revoked',
-        'error': 'token_revoked'
-    }), 401
+jwt_init(app)
 
 api.add_resource(UserRegister, '/register')
 api.add_resource(UserLogin, '/login')
